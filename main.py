3c1d12e458392f045628bb3cd437dc2cb4fe05a7
espce <55976695+espce@users.noreply.github.com>
2024-12-08 20:02:48 +0900

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import langchain_openai
 from langchain_openai import ChatOpenAI
 
-openai.api_key = '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'
+openai.api_key = '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'
 # ChatOpenAI 객체 생성 시 API 키 전달
 chat_model = ChatOpenAI(api_key=openai.api_key)
 
