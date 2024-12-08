ed98325a0025b24fa069cd79291f3eb213f6a9fa
espce <55976695+espce@users.noreply.github.com>
2024-12-08 20:15:14 +0900

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import openai
 import streamlit as st
 import langchain_openai
+
 from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
 
-openai.api_key = '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'
-# ChatOpenAI 객체 생성 시 API 키 전달
-chat_model = ChatOpenAI(api_key=openai.api_key)
-
-# Streamlit 앱 시작
+import streamlit as st
 st.title("인공지능 시인")
 subject = st.text_input("시의 주제를 입력해주세요.")
 st.write("시의 주제 : " + subject)
